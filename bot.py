7ba8c7487ef7173cbda5613e997c6eefc45ada5e
Evgeny Levin <evglevin@MacBook-Pro-Evgenij.local>
2016-10-11 22:08:23 +0300

    Bot is ready

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
-import bot_config
-import telebot
+from bot_config import TELEGRAM_TOKEN
+import telebot  #Python implementation for the Telegram Bot API
+from message_processing import db_open_connection, db_close_connection, text_processing
 
-bot = telebot.TeleBot(bot_config.telegram_token)
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+def text_answer(message):
+    db_open_connection()
+    ans = text_processing(message.text)
+    db_close_connection()
+    bot.send_message(message.chat.id, ans)
+
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
