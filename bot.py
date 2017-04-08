8f3efe29ca9dd8c41afc5e0d7b14abdb7a4a3cff
Evgeny Levin <leo@MacBook-Pro-Evgenij.local>
2017-04-08 11:22:43 +0300

    Now bot doesn't fall when losing internet connection

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ def voice_processing(message):
         # Handling the case where the detection failed
         bot.send_message(message.chat.id, text='Я вас не понимаю')
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+
+bot.polling(none_stop=True, timeout=86400)
